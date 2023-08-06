d4e031459bf54fb113e6e3b8346c0ae6eb1af66c
Денис Снигирёв <snigiden@eurasia888.ru>
2023-08-06 17:58:17 +0400

    sovsem na review ahaha nakanecta

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
