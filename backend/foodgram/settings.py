081e1e40929fd31b592bc44fcd023c069e9ed56f
Снигирёв Денис <snigiden@eurasia888.ru>
2023-08-10 13:17:36 +0400

    test

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'backend',
-    '84.201.161.194',
+    '158.160.22.121',
     'db'
 ]
 
@@ -76,6 +76,13 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', default=5432)
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 AUTH_PASSWORD_VALIDATORS = [
